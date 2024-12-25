54b968848bc2265d324ad0ec50d4442b497c04f5
hiedra <mjesteve@github.com>
2024-12-25 23:07:54 +0100

    Compile 1.0.46.1

--- dist/virtual-select.js
+++ dist/virtual-select.js
@@ -2805,7 +2805,6 @@ var VirtualSelect = /*#__PURE__*/function () {
         DomUtils.setAria(this.$wrapper, 'expanded', false);
         DomUtils.setAria(this.$wrapper, 'activedescendant', '');
       }
-      this.$wrapper.focus();
       if (this.dropboxPopover && !isSilent) {
         this.dropboxPopover.hide();
       } else {
@@ -3833,11 +3832,6 @@ var VirtualSelect = /*#__PURE__*/function () {
     }
 
     // End Royale
-  }, {
-    key: "setReadOnlyMethod",
-    value: function setReadOnlyMethod(value) {
-      return this.virtualSelect.setReadOnly(value);
-    }
   }, {
     key: "destroyMethod",
     value: function destroyMethod() {
